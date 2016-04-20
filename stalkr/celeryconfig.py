bc81055fc61b81d16a1d74dbabc3dda7a0f01608
Helder Martins <heldergaray@gmail.com>
2016-04-21 00:25:47 +0200

    Added PageRank job

--- stalkr/celeryconfig.py
+++ stalkr/celeryconfig.py
@@ -7,7 +7,11 @@ static_dir = os.path.join(static_dir, 'static')
 CELERYBEAT_SCHEDULE = {
     'importtweets-task': {
         'task': 'stalkr.tasks.import_tweets',
-        'schedule': timedelta(minutes=30)
+        'schedule': timedelta(minutes=50)
+    },
+    'computepagerank-task': {
+        'task': 'stalkr.tasks.compute_pagerank',
+        'schedule': timedelta(minutes=70)
     },
 }
 
