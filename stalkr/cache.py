9d0ba593ae5f7e23a1bd32573ad8e80dac6eb845
antoli <antoli@share-02.csc.kth.se>
2016-05-04 21:32:20 +0200

    Raise exception if the image file extension is unknown

--- stalkr/cache.py
+++ stalkr/cache.py
@@ -1,6 +1,13 @@
 import os
 import requests
 
+class UnknownExtensionException:
+    def __init__(self, extension):
+        self.extension = extension
+
+    def __str__(self):
+        return repr("{0}: unknown extension".format(self.extension))
+
 class Cache:
     extensions = ["gif", "jpeg", "jpg", "png"]
 
@@ -21,6 +28,8 @@ class Cache:
         res = requests.get(url)
         if res.status_code == 200:
             _, extension = os.path.splitext(url)
+            if extension[1:] not in self.extensions:
+                raise UnknownExtensionException(extension[1:])
             filename = key + extension
             path = os.path.join(self.directory, filename)
             with open(path, "wb") as file:
