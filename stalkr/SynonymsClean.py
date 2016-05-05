fab90c442a028e2b93bd7830505f74dabfca0810
Syriniity <ammo-11@hotmail.com>
2016-05-05 16:14:45 +0200

    Get synonym words pycode, cleaning using topics.py

---
+++ stalkr/SynonymsClean.py
@@ -0,0 +1,36 @@
+import word2vec
+import topics
+
+#Functionality using word2vec and nltk to use a previously trained model
+#and from an input word find synonyms and return them in a list.
+#The words are cleaned according to the method get_topics in topics.py.
+
+def get_synonyms(word):
+    #Clean input word (lowercase, stemming, ect)
+    res = topics.get_topics(word)
+
+    cleanedWord = list(res)[0] #python3, map not subscriptable
+    
+    #Load trained model, !-CHANGE PATH TO PATH OF MODEL-!
+    model = word2vec.load("C:/Users/Anna/Desktop/word2vec-master/word2vec-master/vectors.bin")
+
+    try:
+        #get similar words from model
+        indexes, metrics = model.cosine(cleanedWord)
+        synonyms = model.vocab[indexes]
+
+        #Clean data
+        string = ""
+        for elem in synonyms:
+            string = string + " " + elem
+        cleanedSynonyms = topics.get_topics(string)
+        
+        #remove duplicate of search word
+        wordList = list(cleanedSynonyms)
+        while cleanedWord in wordList: wordList.remove(cleanedWord)
+        
+    except KeyError as e:
+        #return empty list if word was not present in model
+        wordList = None
+        
+    return wordList
